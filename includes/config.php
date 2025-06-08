e4bc3ea85147f855db21aba7d72ee3fcedb6462b
Jimmy Garcia <31173781+garushia29@users.noreply.github.com>
2025-06-07 21:07:52 -0400

    security

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'portfolio');
 define('SITE_URL', 'http://localhost/my_portfolio');
 
 // Configuración de zona horaria
-date_default_timezone_set('America/Mexico_City');
+date_default_timezone_set('America/La_Paz');
 
 // Configuración de errores en desarrollo
 ini_set('display_errors', 1);
@@ -17,6 +17,6 @@ error_reporting(E_ALL);
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 define('SMTP_USER', 'garushia29@gmail.com'); // Tu correo
-define('SMTP_PASS', 'pltj maem xebf pvjz'); // Contraseña de aplicación de Gmail
+define('SMTP_PASS', ''); // Contraseña de aplicación de Gmail
 define('SMTP_FROM_NAME', 'Jimmy Dev Portfolio');
 ?>
